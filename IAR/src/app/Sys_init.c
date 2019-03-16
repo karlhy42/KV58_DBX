7fd5fca00ae0ce4191f946885e07c54f27ca15fc
karlhy42 <zhji-383@163.com>
2019-03-16 17:01:34 +0800

    1.2

--- IAR/src/app/Sys_init.c
+++ IAR/src/app/Sys_init.c
@@ -5,5 +5,9 @@ void System_init(void)
   DisableInterrupts;
   SYSMPU->CESR &= ~SYSMPU_CESR_VLD_MASK;
   
+  GPIO_Init(LED2_PORT,GPO,1);           //ÓÃ»§LEDµÆ
+  GPIO_Init(LED1_PORT,GPO,1);
+  GPIO_Init(LED3_PORT,GPO,0);
+  
   EnableInterrupts;  
 }
--- IAR/src/app/define.h
+++ IAR/src/app/define.h
@@ -45,4 +45,63 @@
 #define NRF_CE PTA25_OUT
 #define NRF_IRQ PTA24_OUT
 
+
+//BUTTON
+#define BUT1_PORT       PORTB,16
+#define BUT1            PTB16_IN
+#define BUT2_PORT       PORTB,8
+#define BUT2            PTB8_IN
+#define BUT3_PORT       PORTB,6
+#define BUT3            PTB6_IN
+
+
+//BEEP
+#define BEEP_PORT       PORTD,9
+#define BEEP            PTD9_OUT
+
+//LED
+#define LED1_PORT PORTA,26
+#define LED2_PORT PORTA,27
+#define LED3_PORT PORTA,19
+#define LED1 PTA26_OUT
+#define LED2 PTA27_OUT
+#define LED3 PTA19_OUT
+
+#define LED1_ON        PTA26_OUT = 0
+#define LED2_ON        PTA27_OUT = 0
+#define LED3_ON        PTA19_OUT = 0
+#define LED1_OFF       PTA26_OUT = 1
+#define LED2_OFF       PTA27_OUT = 1
+#define LED3_OFF       PTA19_OUT = 1
+
+   
+//MOTOR
+#define MOTOR_L_FTM     FTM0,CH2
+#define MOTOR_R_FTM     FTM0,CH4
+#define MOTOR_L_DIR_PORT        PORTA,8    
+#define MOTOR_R_DIR_PORT        PORTA,9
+#define MOROT_L_DIR     PTA8_OUT
+#define MOROT_R_DIR     PTA9_OUT
+
+   
+//SW
+#define SW1_PORT PORTA,18
+#define SW2_PORT PORTA,17
+#define SW3_PORT PORTA,15
+#define SW4_PORT PORTA,12
+#define SW5_PORT PORTA,11
+#define SW6_PORT PORTA,10
+#define SW7_PORT PORTA,6
+#define SW8_PORT PORTA,4
+
+#define SW1 PTA18_IN
+#define SW2 PTA17_IN
+#define SW3 PTA15_IN
+#define SW4 PTA12_IN
+#define SW5 PTA11_IN
+#define SW6 PTA10_IN
+#define SW7 PTA6_IN
+#define SW8 PTA4_IN
+
+
 #endif /* DEFINE_H_ */
