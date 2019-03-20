85edb72129bc36addd6c1d43ece78fd3fea13bea
karlhy42 <zhji-383@163.com>
2019-03-20 20:58:27 +0800

    2019.3.20

--- IAR/project/settings/KV58_ZBT.Debug.cspy.bat
+++ IAR/project/settings/KV58_ZBT.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.1\common\bin\cspybat" -f "C:\Users\Administrator.CN-20181005PBLK\Documents\GitHub\KV58_DBX\IAR\project\settings\KV58_ZBT.Debug.general.xcl" --backend -f "C:\Users\Administrator.CN-20181005PBLK\Documents\GitHub\KV58_DBX\IAR\project\settings\KV58_ZBT.Debug.driver.xcl" 
+"D:\IAR\common\bin\cspybat" -f "C:\Users\zhji-\Documents\GitHub\KV58_DBX\IAR\project\settings\KV58_ZBT.Debug.general.xcl" --backend -f "C:\Users\zhji-\Documents\GitHub\KV58_DBX\IAR\project\settings\KV58_ZBT.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.1\common\bin\cspybat" -f "C:\Users\Administrator.CN-20181005PBLK\Documents\GitHub\KV58_DBX\IAR\project\settings\KV58_ZBT.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\Administrator.CN-20181005PBLK\Documents\GitHub\KV58_DBX\IAR\project\settings\KV58_ZBT.Debug.driver.xcl" 
+"D:\IAR\common\bin\cspybat" -f "C:\Users\zhji-\Documents\GitHub\KV58_DBX\IAR\project\settings\KV58_ZBT.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\zhji-\Documents\GitHub\KV58_DBX\IAR\project\settings\KV58_ZBT.Debug.driver.xcl" 
 
 @echo off 
 :end
--- IAR/src/app/define.h
+++ IAR/src/app/define.h
@@ -62,17 +62,17 @@
 //LED
 #define LED1_PORT PORTA,26
 #define LED2_PORT PORTA,27
-#define LED3_PORT PORTA,19
+//#define LED3_PORT PORTA,19
 #define LED1 PTA26_OUT
 #define LED2 PTA27_OUT
-#define LED3 PTA19_OUT
+//#define LED3 PTA19_OUT
 
 #define LED1_ON        PTA26_OUT = 0
 #define LED2_ON        PTA27_OUT = 0
-#define LED3_ON        PTA19_OUT = 0
+//#define LED3_ON        PTA19_OUT = 0
 #define LED1_OFF       PTA26_OUT = 1
 #define LED2_OFF       PTA27_OUT = 1
-#define LED3_OFF       PTA19_OUT = 1
+//#define LED3_OFF       PTA19_OUT = 1
 
    
 //MOTOR
@@ -85,23 +85,23 @@
 
    
 //SW
-#define SW1_PORT PORTA,18
+//#define SW1_PORT PORTA,18
 #define SW2_PORT PORTA,17
 #define SW3_PORT PORTA,15
 #define SW4_PORT PORTA,12
 #define SW5_PORT PORTA,11
 #define SW6_PORT PORTA,10
 #define SW7_PORT PORTA,6
-#define SW8_PORT PORTA,4
+//#define SW8_PORT PORTA,4
 
-#define SW1 PTA18_IN
+//#define SW1 PTA18_IN
 #define SW2 PTA17_IN
 #define SW3 PTA15_IN
 #define SW4 PTA12_IN
 #define SW5 PTA11_IN
 #define SW6 PTA10_IN
 #define SW7 PTA6_IN
-#define SW8 PTA4_IN
+//#define SW8 PTA4_IN
 
 
 #endif /* DEFINE_H_ */
